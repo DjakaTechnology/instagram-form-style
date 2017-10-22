38bc4b560c37e9fd478550aaf991c108be6961d0
DTechnology <djakajaya89@gmail.com>
2017-10-23 00:01:21 +0700

    Added DB
    Added DB

--- XIRPL2_10/form.php
+++ XIRPL2_10/form.php
@@ -1,30 +1,31 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Form Register</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <title>Form Register</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
 <body>
-<form method="POST" id="form" action="hasil.php">
-	<div class="content">
-		<h1>DJagram</h1>
-		<h3>Sign up to see photos and videos<br>from your fiends</h3>
-		<hr width="80%">
-		<button>Login with Facebook</button><br>
-		<p style="margin: 5px;">OR</p>
-		<input type="text" name="email" placeholder="Mobile Number or Email" required><br>
-		<input type="text" name="name" placeholder="Full Name" required><br>
-		<input type="text" name="username" placeholder="Username" required=""><br>
-		<input type="password" name="password" placeholder="Password" required=""><br>
-		<button type="submit">Sign Up</button><br>
-		<p>By signing up, you agree to our<br><b>Terms & Privacy Policy</b>.</p>
-	</div>
-	<div id="bottom">
-		<div class="content">
-			<p>Have an account ? <a href="login">Login</a></p>
-		</div>
-	</div>
-</center>
-</form>
+    <form method="POST" id="form" action="hasil.php">
+        <div class="content">
+            <h1>DJagram</h1>
+            <h3>Sign up to see photos and videos<br>from your fiends</h3>
+            <hr width="80%">
+            <button>Login with Facebook</button><br>
+            <p style="margin: 5px;">OR</p>
+            <input type="email" name="email" placeholder="Mobile Number or Email" required><br>
+            <input type="text" name="name" placeholder="Full Name" required><br>
+            <input type="text" name="username" placeholder="Username" required=""><br>
+            <input type="password" name="password" placeholder="Password" required=""><br>
+            <button type="submit">Sign Up</button><br>
+            <p>By signing up, you agree to our<br><b>Terms & Privacy Policy</b>.</p>
+        </div>
+        <div id="bottom">
+            <div class="content">
+                <p>Have an account ? <a href="login">Login</a></p>
+            </div>
+        </div>
+    </form>
 </body>
-</html>	
+</html>
--- XIRPL2_10/hasil.php
+++ XIRPL2_10/hasil.php
@@ -1,43 +1,80 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Pendaftaran berhasil</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <title>Pendaftaran berhasil</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+<?php
+    $DBServer = 'localhost';
+    $DBUser   = 'root';
+    $DBPass   = '';
+    $DBName   = 'db_instagram_style';
+
+    $conn = new mysqli($DBServer, $DBUser, $DBPass, $DBName);
+    
+    if ($conn->connect_error) {
+        trigger_error('Database connection failed: '  . $conn->connect_error, E_USER_ERROR);
+    }
+
+    $email= $_POST['email'];
+    $username = $_POST['username'];
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+
+    $sql= "INSERT INTO userdata (name, username, password, email) VALUES ('$name', '$username', '$password', '$email')";
+
+    if($conn->query($sql) === false) {
+        trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+    } else {
+        $last_inserted_id = $conn->insert_id;
+        $affected_rows = $conn->affected_rows;
+    }
+?>
 <body>
-<form id="result" method="POST" action="hasil.php">
-	<div class="content">
-		<div class="header">
-			<h1>Djagram</h1>
-		</div>
-		<h3>Registration Complete!!</h3>
-		<hr width="80%">
-		<table width="320px">
-			<col width="45%">
-			<col width="3%">
-			<col width="52%">
-			<tr>
-				<td>Mobile Number or Email</td>
-				<td>:</td>
-				<td><?php echo $_POST["email"]; ?></td>
-			</tr>
-			<tr>
-				<td>Full Name</td>
-				<td>:</td>
-				<td><?php echo $_POST["name"]; ?></td>
-			</tr>
-			<tr>
-				<td>Username</td>
-				<td>:</td>
-				<td><?php echo $_POST["username"]; ?></td>
-			</tr>
-			<tr>	
-				<td>Password</td>
-				<td>:</td>
-				<td><?php echo $_POST["password"]; ?></td>
-			</tr>
-		</table>
-		<a href="form.php"><button type="button">Back</button></a>
-	</div>
+    <form id="result" method="POST" action="hasil.php">
+        <div class="content">
+            <div class="header">
+                <h1>Djagram</h1>
+            </div>
+            <h3>Registration Complete!!</h3>
+            <hr width="80%">
+            <table width="320px">
+                <col width="45%">
+                <col width="3%">
+                <col width="52%">
+                <tr>
+                    <td>Mobile Number or Email</td>
+                    <td>:</td>
+                    <td>
+                        <?php echo $_POST["email"]; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Full Name</td>
+                    <td>:</td>
+                    <td>
+                        <?php echo $_POST["name"]; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Username</td>
+                    <td>:</td>
+                    <td>
+                        <?php echo $_POST["username"]; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td>:</td>
+                    <td>
+                        <?php echo $_POST["password"]; ?>
+                    </td>
+                </tr>
+            </table>
+            <a href="form.php"><button type="button">Back</button></a>
+        </div>
+    </form>
 </body>
+
 </html>
